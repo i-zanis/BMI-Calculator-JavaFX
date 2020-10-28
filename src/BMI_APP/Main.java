99fb0b29cee1cb42a184f346bf87babf8b2ae693
i-zanis <ioanlafazanis@gmail.com>
2020-10-28 23:38:35 +0000

    11382810

--- src/BMI_APP/Main.java
+++ src/BMI_APP/Main.java
@@ -25,17 +25,21 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        // initial values to window size for quick rescaling
+        // initial values of window size for quick rescaling
         int windowWidth = 345;
         int windowHeight = 505;
         Pane pane = new Pane();
         pane.setPrefSize(windowWidth, windowHeight);
 
+        // Settings of top partition
+
+        // Creation of the white background (rectangle), top black rectangle, mid rectangle
         Rectangle rectangleBase = new Rectangle(windowWidth, windowHeight, Color.WHITE);
         Rectangle rectangleTop = new Rectangle(windowWidth, windowHeight / 10f, Color.BLACK);
         Rectangle rectangleMid = new Rectangle(windowWidth, windowHeight / 11f, Color.BLACK);
         rectangleMid.setY(278);
 
+        // title BMI calculator at the top of the window
         Label topLabel = new Label("BMI Calculator");
         topLabel.setFont(Font.font("Bauhaus 93", 30));
         topLabel.setTextFill(Color.WHITE);
@@ -43,60 +47,71 @@ public class Main extends Application {
         topLabel.setLayoutX(72.0);
         topLabel.setTranslateY(9.0);
 
+        // Settings for Calculate button
         Button calculateButton = new Button("Calculate");
         calculateButton.setStyle("-fx-background-color: white; -fx-border-color: black; -fx-border-radius: 25;");
         calculateButton.setFont(Font.font("Bauhaus 93", 20));
         calculateButton.setLayoutX(202.0);
         calculateButton.setTranslateY(208.0);
 
+        // Settings for error label on invalid input under calculateButton/above weightLabel
         Label errorLabel = new Label();
         errorLabel.setFont(Font.font(15));
         errorLabel.setTextFill(Color.RED);
         errorLabel.setLayoutX(24.0);
         errorLabel.setTranslateY(90.0);
 
+        // Settings for label displaying "Weight": on the left
         Label weightLabel = new Label("Weight:");
         weightLabel.setFont(Font.font(18));
         weightLabel.setLayoutX(24.0);
         weightLabel.setTranslateY(118.0);
 
+        // Settings for label displaying "Height" on the left
         Label heightLabel = new Label("Height:");
         heightLabel.setFont(Font.font(18));
         heightLabel.setLayoutX(26.0);
         heightLabel.setTranslateY(158.0);
 
+        // Settings for TextField next to weightLabel
         TextField tfWeight = new TextField();
         tfWeight.setFont(Font.font(12));
         tfWeight.setLayoutX(107.0);
         tfWeight.setTranslateY(118.0);
         tfWeight.setStyle("-fx-background-radius: 8;");
-        tfWeight.setPromptText("Enter weight");
+        tfWeight.setPromptText("Enter weight"); // gray text implying input
 
+        // Settings for TextField next to heightLabel
         TextField tfHeight = new TextField();
         tfHeight.setFont(Font.font(12));
         tfHeight.setLayoutX(107.0);
         tfHeight.setTranslateY(158.0);
         tfHeight.setStyle("-fx-background-radius: 8;");
-        tfHeight.setPromptText("Enter height");
+        tfHeight.setPromptText("Enter height"); // gray text implying input
 
-        ChoiceBox<String> choiceBoxWeight = new ChoiceBox<>(FXCollections.observableArrayList(
-                "lbs", "kg"));
-        choiceBoxWeight.setValue("lbs"); //initialize value to LBS
+        // Settings for Options for height Imperial/Metric on the right of tfWeight
+        ChoiceBox<String> choiceBoxWeight = new ChoiceBox<>(FXCollections.observableArrayList("lbs", "kg"));
+        choiceBoxWeight.setValue("lbs"); // set initial value to lbs
         choiceBoxWeight.setTooltip(new Tooltip("Select Metric or Imperial system."));
         choiceBoxWeight.setLayoutX(272);
         choiceBoxWeight.setTranslateY(118);
-        choiceBoxWeight.setStyle("-fx-background-color: white; -fx-border-color: #0a151b #0a151b; -fx-border-radius: 8; -fx-pref-width: 45;");
+        choiceBoxWeight.setStyle("-fx-background-color: white; -fx-border-color: #0a151b #0a151b; " +
+                "-fx-border-radius: 8; -fx-pref-width: 45;");
 
-        ChoiceBox<String> choiceBoxHeight = new ChoiceBox<>(FXCollections.observableArrayList(
-                "in", "cm"));
-        choiceBoxHeight.setValue("in");    //initialize value to inch
+        // Settings for Options for height Imperial/Metric on the right of tfHeight
+        ChoiceBox<String> choiceBoxHeight = new ChoiceBox<>(FXCollections.observableArrayList("in", "cm"));
+        choiceBoxHeight.setValue("in");    // set initial value to inch
         choiceBoxHeight.setTooltip(new Tooltip("Select Metric or Imperial system."));
         choiceBoxHeight.setLayoutX(272);
         choiceBoxHeight.setTranslateY(158.0);
-        choiceBoxHeight.setStyle("-fx-background-color: white; -fx-border-color: #0a151b #0a151b; -fx-border-radius: 8; -fx-pref-width: 45;");
+        choiceBoxHeight.setStyle("-fx-background-color: white; -fx-border-color: #0a151b #0a151b; " +
+                "-fx-border-radius: 8; -fx-pref-width: 45;");
+        // End of Settings for top partition
 
 
-        // Results Partition
+        // Start of second partition Settings "Results"
+
+        // Settings for middle center label "BMI Result"
         Label midLabel = new Label("BMI Result");
         midLabel.setFont(Font.font("Bauhaus 93", 30));
         midLabel.setTextFill(Color.WHITE);
@@ -105,7 +120,7 @@ public class Main extends Application {
         midLabel.setLayoutX(95.0);
         midLabel.setTranslateY(284.0);
 
-
+        // Settings for finalResult label under midLabel -shows final BMI result
         Label finalResult = new Label();
         finalResult.setFont(Font.font("Arial", FontWeight.BOLD, 25));
         finalResult.setTextFill(Color.BLACK);
@@ -114,6 +129,7 @@ public class Main extends Application {
         finalResult.setLayoutX(80);
         finalResult.setTranslateY(335);
 
+        // Settings for displayed suggesting message under finalResult
         Label bmiMessage = new Label();
         bmiMessage.setFont(Font.font(15));
         bmiMessage.setTextFill(Color.BLACK);
@@ -123,19 +139,22 @@ public class Main extends Application {
         bmiMessage.setPrefWidth(260);
         bmiMessage.setWrapText(true);
 
-
+        // Settings for resetButton that appears after calculateButton is pressed
         Button resetButton = new Button("Reset");
         resetButton.setStyle("-fx-background-color: white; -fx-border-color: black; -fx-border-radius: 25;");
         resetButton.setFont(Font.font("Bauhaus 93", 20));
         resetButton.setLayoutX(224.0);
         resetButton.setTranslateY(450.0);
         resetButton.setVisible(false);
+        // End of second partition Settings "Results"
 
-
+        // Addition of all nodes to pane. New nodes won't display if not added here
         pane.getChildren().addAll(rectangleBase, rectangleTop, rectangleMid, topLabel, errorLabel, calculateButton, weightLabel, heightLabel,
                 tfWeight, tfHeight, choiceBoxWeight, choiceBoxHeight, midLabel, finalResult, bmiMessage, resetButton);
 
+        // Start of Event settings for buttons
 
+        // Event handler for the calculateButton
         EventHandler<ActionEvent> calculateButtonPress = new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -161,7 +180,7 @@ public class Main extends Application {
                         tfHeight.setText(""); //clear height text field
                         tfHeight.requestFocus();
                     }
-                    if (height == 0) throw new ArithmeticException(); // to prevent displaying infinity in finalResult
+                    if (height == 0.0) throw new ArithmeticException(); // to prevent displaying infinity in finalResult
 
                     Double bmi = calculateBMI(weight, height); //calculate user BMI
                     resultBMI(bmi, finalResult, bmiMessage); //display result to user
@@ -181,6 +200,7 @@ public class Main extends Application {
             }
         };
 
+        // Event handler for resetButton
         EventHandler<ActionEvent> resetButtonPress = new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -194,10 +214,12 @@ public class Main extends Application {
             }
         };
 
+        // Setting event source objects
         calculateButton.setOnAction(calculateButtonPress);
         //to put enter key
         resetButton.setOnAction(resetButtonPress);
 
+        // End of Event settings for buttons
 
         Scene scene = new Scene(pane, windowWidth, windowHeight);
         //Parent root = FXMLLoader.load(getClass().getResource("BMI.fxml"));
